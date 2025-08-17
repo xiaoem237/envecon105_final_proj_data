2ddf974fd5d83fe6907336e5dba8e2cbb0206f5d
xiaoem237 <143116454+xiaoem237@users.noreply.github.com>
2025-08-16 22:58:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write("Author: Emily Xiao")
 
 #with st.echo():
 #  st.markdown("(static/world_co2.png)")
-(static/world_co2.png)
+https://github.com/xiaoem237/envecon105_final_proj_data/blob/main/static/world_co2.png?raw=true
