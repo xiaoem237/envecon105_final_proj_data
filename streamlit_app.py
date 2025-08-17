d8d64937fb3ea18bee337f87f5bd1c10cab4e5b3
xiaoem237 <143116454+xiaoem237@users.noreply.github.com>
2025-08-16 22:50:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title("Envecon 105 Case Study Dashboard")
 st.write("Author: Emily Xiao")
 
-st.markdown("(./app/static/world_co2.png)")
+with st.echo():
+  st.markdown("(./app/static/world_co2.png)")
