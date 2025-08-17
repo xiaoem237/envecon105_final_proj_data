e01cedb7a085c20489e0d1178442e10f15c816c4
xiaoem237 <143116454+xiaoem237@users.noreply.github.com>
2025-08-16 23:40:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,18 @@ st.write("Here is a plot illustrating more detailed the trends of the top 10 CO2
 
 st.write("Now, let's take a look at the CO2 emissions, GDP per capita, and energy use of the United States compared to other countries:")
 st.image("https://github.com/xiaoem237/envecon105_final_proj_data/blob/main/static/indicators_vs_US.png?raw=true")
-st.write("")
 
 st.write("Let's do the same for Switzerland:")
 st.image("https://github.com/xiaoem237/envecon105_final_proj_data/blob/main/static/indicators_vs_Switzerland.png?raw=true")
+
+st.write("Neither of these countries seem to have odd trends of these variables, besides the U.S. that has very large CO2 emissions.")
+
+st.write("Another interest of this case study was the relationship between CO2 emissions and average annual temperature. Let's take a look at the trends of these variables for both countries.")
+st.image("https://github.com/xiaoem237/envecon105_final_proj_data/blob/main/static/co2_temp_us.png?raw=true")
+st.write("In these two plots, it seems that the regression lines offer a similar shape, both variables peaking in the 2000s and falling in more recent years.")
+st.image("https://github.com/xiaoem237/envecon105_final_proj_data/blob/main/static/co2_temp_switzerland.png?raw=true")
+st.write("Switzerland, however, is quite different. It arguably does have a small peak around the year 2010 for CO2 emissions like the U.S., but the temperature continues to curve upward instead of matching the trend of the emissions.")
+
+st.write("Lastly, we can take a look at a scaled scatterplot for CO2 emissions and temperature for both countries to get a better idea of the association between them:")
+st.image("https://github.com/xiaoem237/envecon105_final_proj_data/blob/main/static/us_co2_temp_scatter.png?raw=true")
+st.image("https://github.com/xiaoem237/envecon105_final_proj_data/blob/main/static/switzerland_co2_temp_scatter.png?raw=true")
